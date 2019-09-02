1383bf630c07b32af72c50ffd09ef08364088db3
xiaozhuang18 <1448680248@qq.com>
2019-09-02 21:08:05 +0800

    字典哈希表

--- 1501.js
+++ 1501.js
@@ -1,9 +1,9 @@
-/**
- * Created by 14486 on 2019/4/4.
- */
 /*
-* 字典
-*   键值存储数据 [键,值]*/
+Created by 14486 on 2019/9/2.
+    字典 类似于map数据结构
+        键值存储数据 [键,值]
+*/
+// 模拟代码,没有什么实际意义,实际使用直接用map就可以了
 let Map = (function () {
     let sym = Symbol();
     return class{
@@ -14,12 +14,13 @@ let Map = (function () {
         }
         set(key,value){
             // 判断是否存在key
+            // 不存在就 push
+            // 不用直接循环性能更好 flag也要用到for循环
             let flag = this.has(key);
             if(flag){
                 this[sym].forEach(v=>{
                     if(v[0]===key){
                         v[1] = value;
-                        return;
                     }
                 })
             }else{
@@ -30,7 +31,7 @@ let Map = (function () {
             let length = this.size();
             for (var i = 0; i < length ; i++) {
                 if(this[sym][i][0]===key){
-                    return [this[sym][i][1],i];
+                    return [this[sym][i][0],this[sym][i][1]];
                 }
             }
         }
@@ -38,12 +39,9 @@ let Map = (function () {
             let i = false;
             this[sym].forEach(v=>{
                 if(v[0]===key){
-                    i = true;
+                    return true;
                 }
-            })
-            if(i){
-                return true;
-            }
+            });
             return false;
         }
         delete(key){
@@ -60,18 +58,26 @@ let Map = (function () {
     }
 })();
 let map = new Map();
-
+// 并不是很方便 存储值和取值 都要遍历
 /*
-* 哈希表
-*   哈希函数*/
+哈希表 最重要的就是哈希函数
+    哈希函数
+        key => value
+        怎么样将key转化为一个数字,存值的时候通过它存值,取也是取值;
+        因为数组存取值直接找到对应序号就可以了
+        解决办法 s = "" + str.charCodeAt(0) - 0; 得到一个数字
+
+        具有唯一性,简单,
+*/
 let HashTab = (function () {
     let sym = Symbol();
     function hash(key) {
-        let num = "";
+        let num = 0;
         [...key].forEach(item=>{
             num+=item.charCodeAt(0);
         });
-        
+        // 需要解决问题  afei 和 feia重复 数组长度过长( 用% )
+        // 用链表解决
         return num;
     }
     return class{
